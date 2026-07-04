3070d38d4b84f5f93ddf4afa7e051265414e5c33
Hassan <devitquickhassan@gmail.com>
2026-07-05 03:55:36 +0500

    eas build

--- mobile/src/constants/config.ts
+++ mobile/src/constants/config.ts
@@ -1,7 +1,12 @@
 // src/constants/config.ts
 
 
+// export const CONFIG = {
+//   API_URL:
+//     "http://192.168.100.17:5000/api",
+// };
+
 export const CONFIG = {
   API_URL:
-    "http://192.168.100.17:5000/api",
+    "https://attractive-eagerness-production-4fc0.up.railway.app/api",
 };
