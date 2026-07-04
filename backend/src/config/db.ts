1341a57ff4be31d416c7c7910ac7afe8013ebfde
Hassan <devitquickhassan@gmail.com>
2026-07-04 12:18:37 +0500

    db updated

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,27 @@
+// import mongoose from "mongoose";
+
+// export async function connectDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI!);
+
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error("Database connection failed");
+
+//     process.exit(1);
+//   }
+// }
+
 import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI!);
 
-    console.log("MongoDB connected");
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("Database connection failed");
+    console.error("❌ Database connection failed:");
+    console.error(error);
 
     process.exit(1);
   }
